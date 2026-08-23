37411a641c58f8b56926f7136d63a8fc08f375d0
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2026-08-23 13:40:32 +0200

    add MSVC build script

---
+++ build_msvc.bat
@@ -0,0 +1,16 @@
+@echo off
+
+for /f "usebackq delims=" %%V in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+    call "%%V\VC\Auxiliary\Build\vcvars64.bat"
+)
+
+if not exist build mkdir build
+
+for %%F in (
+    source\capture-detector.c
+    source\_test_protected.c
+) do (
+    cl "%%F" %FLAGS% /Fo:"build\%%~nF.obj" /Fe:"build\%%~nF.exe" /link user32.lib
+)
+
+pause
