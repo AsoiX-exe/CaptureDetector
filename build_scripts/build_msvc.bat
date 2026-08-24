6203645ce1fdb8869ae4f7d17a2428a860490dba
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2026-08-24 19:07:36 +0200

    update build_msvc for build_scripts change

--- build_scripts/build_msvc.bat
+++ build_scripts/build_msvc.bat
@@ -1,10 +1,7 @@
 @echo off
+setlocal
 
 cd /d "%~dp0.."
-
-for /f "usebackq delims=" %%V in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
-    call "%%V\VC\Auxiliary\Build\vcvars64.bat"
-)
 
 if not exist build mkdir build
 
@@ -12,7 +9,19 @@ for %%F in (
     source\capture-detector.c
     source\_test_protected.c
 ) do (
-    cl "%%F" %FLAGS% /MT /O2 /Fo:"build\%%~nF.obj" /Fe:"build\%%~nF.exe" /link user32.lib
+    cl "%%F" %FLAGS% /MT /O2 ^
+        /Fo:"build\%%~nF.obj" ^
+        /Fe:"build\%%~nF.exe" ^
+        /link user32.lib kernel32.lib
+
+    if errorlevel 1 (
+        echo.
+        echo Build failed: %%F
+        pause
+        exit /b 1
+    )
 )
 
+echo.
+echo Build successful.
 pause
